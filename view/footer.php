8d2ab042cca69a5d440c68d5b57907183d10a43c
Axel Tanner <axeltanner38@gmail.com>
2014-11-03 13:32:14 -0800

    css

--- view/footer.php
+++ view/footer.php
@@ -1,2 +1,5 @@
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="js/base.js"></script>
 </body>
 </html>
--- view/header.php
+++ view/header.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<title></title>
 </head>
 <body>
