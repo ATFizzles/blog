1a81c60d619a0c886bfdd5f1354fae137ff025ab
Axel Tanner <axeltanner38@gmail.com>
2014-11-03 13:16:43 -0800

    16

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
 <?php
+	//adds the header html needed
+	require_once(__DIR__ . "/view/header.php");
 	//direct connection to the database
 	require_once(__DIR__ . "/controller/create-db.php");
+	//adds the footer html needed
+	require_once(__DIR__ . "/view/footer.php");
 ?>
-</body>
-</html>
