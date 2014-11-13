c0bfc22d827ece0b5040d19b2b157705a9364177
Axel Tanner <axeltanner38@gmail.com>
2014-11-13 12:06:48 -0800

    vid 24: added query for database now submit button actually submits info to the database

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,6 @@
 <?php
 	//connects to database.php file
-	require_once(__DIR__ . "/..model/database.php");
+	require_once(__DIR__ . "/../model/database.php");
 
 	//created new mysqli object
 	//created connection to the database
@@ -12,8 +12,20 @@
 	//sanitizing by making sure it is a string
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-	echo "<p>Title: $title</p>";
-	echo "<p>Post: $post</p>";
+	
+	//query is a method
+	//INSERT INTO adds info into the specific tables
+	//INSERT INTO is an action
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+
+	//if query is successful it outputs the echo
+	if ($query) {
+		echo "<p>Successfully inserted post: $title</p>";
+	}
+	//if query wasn't successful it outputs this echo
+	else{
+		echo "<p>$connection->error</p>";
+	}
 
 	//closes connection
 	$connection->close();
