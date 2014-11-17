63cddaa85280f8f12a930649f5159f32506458cc
Axel Tanner <axeltanner38@gmail.com>
2014-11-17 13:21:22 -0800

    vid 27&28: created __construct function, openConnection function, closeConnection function, and query function

--- model/Database.php
+++ model/Database.php
@@ -9,4 +9,31 @@
 		private $username;
 		private $password;
 		private $database;
+
+		//correct way to define your constructor variables
+		//_construct allows you to build an object
+		//accepted parameters are passed when the object is created
+		public function __construct($host, $username, $password, $database){
+			//storing info by assigning info to global variables
+			//constructor variables
+			$this->host = $host;
+			$this->username = $username;
+			$this->password = $password;
+			$this->database = $database;
+		}
+
+		//functions get rid of excess repetition
+		//opens connection
+		public function openConnection(){
+
+		}
+		//closes connectiom
+		public function closeConnection(){
+
+		}
+
+		//string always passed as a variable
+		public function query($string){
+
+		}
 	}
